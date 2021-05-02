e0529b030668c07e98633c6759aeb485a2607bff
Adimazuz <mazuz0992@gmail.com>
2021-05-02 17:35:47 +0300

    one more

--- src/components/experience_sections.jsx
+++ src/components/experience_sections.jsx
@@ -1,22 +1,22 @@
 import ExperienceEntry from "./Experience_entry";
 // import experience_data from "../data/experince_data";
 
-function createExperinceEntry(entry) {
-  return (
-    <ExperienceEntry
-      company={entry.company}
-      product={entry.department}
-      dates={entry.dates}
-      description={entry.description}
-      bullet_points={entry.bullet_points}
-    />
-  );
-}
+// function createExperinceEntry(entry) {
+//   return (
+//     <ExperienceEntry
+//       company={entry.company}
+//       product={entry.department}
+//       dates={entry.dates}
+//       description={entry.description}
+//       bullet_points={entry.bullet_points}
+//     />
+//   );
+// }
 
 function Experience_section(props) {
-  //   const div_style = {
-  //     backgroundColor: props.color,
-  //   };
+  const div_style = {
+    backgroundColor: props.color,
+  };
 
   return (
     <div id="experience" class="container my-rounded section" style={div_style}>
