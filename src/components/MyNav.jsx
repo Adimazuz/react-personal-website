06dac5ada5bd1ee945ba473f40b647b8596a7963
Adimazuz <mazuz0992@gmail.com>
2021-05-02 11:48:39 +0300

    fixed navbar collapse

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,27 +1,30 @@
 function MyNav(props) {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top navbar-center my-nav">
-      <div class="container-fluid">
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light sticky-top my-nav">
         <button
           class="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ml-auto ">
+        <div
+          class="collapse navbar-collapse justify-content-center order-2 "
+          id="navbarNavAltMarkup"
+        >
+          <ul class="navbar-nav  ">
             <li class="nav-item ">
               <a class="nav-link" aria-current="page" href="#home">
                 Home
               </a>
             </li>
             <li class="nav-item ">
-              <a class="nav-link " href="#about">
+              <a class="nav-item nav-link " href="#about">
                 About
               </a>
             </li>
@@ -42,8 +45,8 @@ function MyNav(props) {
             </li>{" "}
           </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
