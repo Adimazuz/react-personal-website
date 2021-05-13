cb89c5ca048069b532976f4e7ad82230df94a2c2
Adimazuz <mazuz0992@gmail.com>
2021-05-13 15:03:06 +0300

    downloded icons

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -19,32 +19,12 @@ export default function Skillssection(props) {
           </div>
           <div class="col-md-4 skills-col">
             <h3>Familiar With</h3>
-            <SkillEntry
-              text="HTML"
-              alt="html-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/888/888859.svg?token=exp=1620889326~hmac=0953399c2152a6d747c028d905648b90"
-            />
-            <SkillEntry
-              text="CSS"
-              alt="css-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/732/732190.svg?token=exp=1620889334~hmac=bc0826261050e383fa26742cd862571f"
-            />
+            <SkillEntry text="HTML" alt="html-img" img="res/html.svg" />
+            <SkillEntry text="CSS" alt="css-img" img="res/css.svg" />
 
-            <SkillEntry
-              text="JavaScript"
-              alt="js-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/136/136530.svg?token=exp=1620889351~hmac=9a88be4e9ebe737833bf21de43c4c72b"
-            />
-            <SkillEntry
-              text="Node.js"
-              alt="nodejs-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/919/919825.svg?token=exp=1620890037~hmac=04f3ea2624e954952a335dfee49827b1"
-            />
-            <SkillEntry
-              text="React"
-              alt="react-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/1126/1126012.svg?token=exp=1620889924~hmac=2aee5e6ae81a67bce2219048ea1878d1"
-            />
+            <SkillEntry text="JavaScript" alt="js-img" img="res/js.svg" />
+            <SkillEntry text="Node.js" alt="nodejs-img" img="res/node.svg" />
+            <SkillEntry text="React" alt="react-img" img="res/react.svg" />
             <SkillEntry
               text="MongoDB"
               alt="mongodb-img"
@@ -56,22 +36,22 @@ export default function Skillssection(props) {
             <SkillEntry
               text="Data Structures"
               alt="Data-Structures-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/3079/3079384.svg?token=exp=1620890328~hmac=037f035e803b57147bccc02d163b1a51"
+              img="res/ds.svg"
             />
             <SkillEntry
               text="Algorithms"
               alt="Algorithms-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/1628/1628175.svg?token=exp=1620890360~hmac=5b8a56baadfc9d3eadec8986c7a804c4"
+              img="res/alg.svg"
             />
             <SkillEntry
               text="Operating systems"
               alt="os-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/2172/2172894.svg?token=exp=1620890381~hmac=b884d6507ecc0b31063ea44fe136ce92"
+              img="res/os.svg"
             />
             <SkillEntry
               text="Computer security"
               alt="Computer-security-img"
-              img="https://www.flaticon.com/svg/vstatic/svg/3208/3208680.svg?token=exp=1620890422~hmac=5eaa1cf6f6c883945305c75b53418843"
+              img="res/csec.svg"
             />
           </div>
         </div>
